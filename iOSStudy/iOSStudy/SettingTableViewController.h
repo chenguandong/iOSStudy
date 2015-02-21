740f5ab1b2265d3ec855f61f5b1044742b460900
陈冠东 <chenguandong@outlook.com>
2015-02-22 00:55:15 +0800

    增加设置界面
    增加设置界面用 使用的是Storyboard+staticCell

---
+++ iOSStudy/iOSStudy/SettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewController.h
+//  iOSStudy
+//
+//  Created by chenguandong on 15/2/21.
+//  Copyright (c) 2015年 chenguandong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UITableViewController
+
+@end
