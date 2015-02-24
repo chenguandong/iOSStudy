6c5675456134b5fd67ea6c986a25e6af69d91c3b
陈冠东 <chenguandong@outlook.com>
2015-02-24 18:10:16 +0800

    添加增加博客界面
    增加博客界面采用的是staticCell 表单样式

---
+++ iOSStudy/iOSStudy/AddBlogTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddBlogTableViewController.h
+//  iOSStudy
+//
+//  Created by chenguandong on 15/2/24.
+//  Copyright (c) 2015年 chenguandong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddBlogTableViewController : UITableViewController
+
+@end
