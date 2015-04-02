d2cd8a0ecb4d22c760bcd4d02dfd5f6808b2f111
陈冠东 <chenguandong@outlook.com>
2015-04-02 14:17:12 +0800

    更新blog数据结构增加版本号

--- iOSStudy/iOSStudy/Constants .h
+++ iOSStudy/iOSStudy/Constants .h
@@ -14,9 +14,19 @@
 #define Address_blogs @"https://raw.githubusercontent.com/chenguandong/iOSStudy/master/iOSStudyJsonData/blogs.json"
 #define Adress_webs @"https://raw.githubusercontent.com/chenguandong/iOSStudy/master/iOSStudyJsonData/webs.json"
 #define Adress_videos @"https://raw.githubusercontent.com/chenguandong/iOSStudy/master/iOSStudyJsonData/videos.json"
+
+#define Adress_versions @"https://raw.githubusercontent.com/chenguandong/iOSStudy/CoreDataJoin/iOSStudyJsonData/versions.json"
+
 #define SharedApp ((AppDelegate *)[[UIApplication sharedApplication] delegate])
 
 #define PATH_OF_DOCUMENT    [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
-
+/**
+ *  收藏的数据库表明
+ */
 #define CD_FAVOURITE_BEAN @"FavouriteBean"
+
+/**
+ *  http版本号数据库表明
+ */
+#define CD_VersionsEntity @"VersionsEntity"
 #endif
